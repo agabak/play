6acee26a21b1aa6d55aef17de1407f8add2e1e95
Agaba Kabunga <agaba_k@hotmail.com>
2021-08-17 16:45:48 -0500

    configure appsetting.json

--- Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -7,29 +7,43 @@ using Microsoft.OpenApi.Models;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 using Play.Catalog.Service.Repositories;
+using Play.Catalog.Service.Settings;
 
 namespace Play.Catalog.Service
 {
     public class Startup
     {
+        private  ServiceSettings serviceSettings;
+        private readonly IConfiguration _config;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _config = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+
+            serviceSettings = _config.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+            // you want to have the same instance at ll time
+            services.AddSingleton(serviceProvider => 
+            {
+                var mongodbSettings =
+                _config.GetSection(nameof(MongodbSettings)).Get<MongodbSettings>();
+                var mongoClient = new MongoClient(mongodbSettings.ConnectionString);
+                return mongoClient.GetDatabase(serviceSettings.ServiceName);
+            } );
+
+            services.AddScoped<IItemsRepository, ItemsRepository>();
+
             services.AddControllers(opts =>
             {
                 opts.SuppressAsyncSuffixInActionNames = false;
             });
-            services.AddScoped<IItemsRepository, ItemsRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Play.Catalog.Service", Version = "v1" });
