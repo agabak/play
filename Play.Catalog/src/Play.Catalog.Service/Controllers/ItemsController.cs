9c6e7fbb08c6b3c313d17e8fb6d04ef7fc605827
Agaba Kabunga <agaba_k@hotmail.com>
2021-08-18 15:02:42 -0500

    Create nuget package for  Common code

--- Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ Play.Catalog/src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Entities;
-using Play.Catalog.Service.Repositories;
+using Play.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
